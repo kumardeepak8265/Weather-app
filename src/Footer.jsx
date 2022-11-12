9eda939d83f29e4dbbd3b8a6ee0f3b2adbbec15b
deepak kumar <kumardeepak6830@gmail.com>
2022-11-12 23:21:42 +0530

    footer heading change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className=" py-4  flex justify-center items-center m-auto">
       <h1 className="text-gray-500">
-        made by team code yogi member deepak kumar
+        made by team code yogi student deepak kumar
       </h1>
     </div>
   );
